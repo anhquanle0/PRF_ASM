5faa248643846579af8e7aba2a302c71236db301
anhquan <quanlafx19871@funix.edu.vn>
2025-09-12 10:04:45 +0700

    ASM3 Class User

--- asm3/scripts/storage.js
+++ asm3/scripts/storage.js
@@ -1,1 +1,13 @@
-'use strict'
+"use strict";
+
+function saveToStorage(k, v) {
+  if (v instanceof Map) {
+    localStorage.setItem(k, JSON.stringify(Array.from(v.values())));
+  } else {
+    localStorage.setItem(k, JSON.stringify(v));
+  }
+}
+
+function getFromStorage(k) {
+  return JSON.parse(localStorage.getItem(k));
+}
