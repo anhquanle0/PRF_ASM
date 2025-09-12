c7123cc47ab5fa6d231faa39ffeeda29d4bbaabe
anhquan <quanlafx19871@funix.edu.vn>
2025-09-12 20:53:51 +0700

    ASM3 Render news

--- asm3/models/User.js
+++ asm3/models/User.js
@@ -2,6 +2,8 @@
 
 const KEY = "USER_ARRAY";
 
+const API_KEY = "4374df249925490cb3ad9a565d7fac29";
+
 class User {
   constructor(firstName, lastName, username, password) {
     this.firstName = firstName;
@@ -12,5 +14,19 @@ class User {
 
   static from({ firstName, lastName, username, password }) {
     return new User(firstName, lastName, username, password);
+  }
+
+  static async getData(page) {
+    const url = `https://newsapi.org/v2/everything?q=*&sortBy=publishedAt&pageSize=10&page=${page}&language=en&apiKey=${API_KEY}`;
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) throw new Error(`Response status: ${response.status}`);
+
+      const { articles } = await response.json();
+      return articles;
+    } catch (err) {
+      console.error("Fetch error:", err);
+    }
   }
 }
--- asm3/scripts/news.js
+++ asm3/scripts/news.js
@@ -1,1 +1,61 @@
-'use strict'
+"use strict";
+
+const newsContainer = document.querySelector("#news-container");
+
+const btnPrev = document.querySelector("#btn-prev");
+const pageNumber = document.querySelector("#page-num");
+const btnNext = document.querySelector("#btn-next");
+
+// fetch("https://newsapi.org/v2/everything?q=*&sortBy=publishedAt&pageSize=100&language=en&apiKey=4374df249925490cb3ad9a565d7fac29")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.json();
+//   })
+//   .then(({ articles }) => {
+//     articles.forEach((el) => console.log(el.title));
+//   })
+//   .catch((error) => {
+//     console.error("Fetch error:", error);
+//   });
+
+let data = [];
+
+User.getData(1).then((res) => {
+  data = [...res];
+
+  renderNew(data);
+});
+
+function renderNew(news) {
+  newsContainer.innerHTML = "";
+
+  news.forEach(({ author, content, description, publishedAt, source, title, url, urlToImage }) => {
+    if (!urlToImage) return;
+
+    const html = `
+            <div class="card flex-row flex-wrap">
+            <div class="card mb-3" style="">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img
+                    src="${urlToImage}"
+                    class="card-img"
+                    alt="${title}" />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <a href="${url}" class="btn btn-primary">View</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  `;
+
+    newsContainer.insertAdjacentHTML("beforeend", html);
+  });
+}
