529dd048bddbeeee79381aa9fbc9574b5a1cd8b0
anhquan <quanlafx19871@funix.edu.vn>
2025-09-12 18:06:07 +0700

    ASM3 Home Page

--- asm3/scripts/home.js
+++ asm3/scripts/home.js
@@ -1,1 +1,16 @@
-'use strict'
+"use strict";
+
+const loginModal = document.querySelector("#login-modal");
+const main = document.querySelector("#main-content");
+
+const message = document.querySelector("#welcome-message");
+
+const curUser = getFromStorage("CUR_USER");
+
+if (!curUser) {
+  main.style.display = "none";
+} else {
+  loginModal.style.display = "none";
+
+  message.innerText = `Welcome back, ${User.from(curUser).firstName}`;
+}
